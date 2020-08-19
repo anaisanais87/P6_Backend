a725ced024ca147f7396f1bd54a2b1529e246235
Anaïs Bertrand <anais.bertrand87@gmail.com>
2020-08-20 00:09:10 +0200

    add files backend

---
+++ controllers/sauces.js
@@ -0,0 +1,50 @@
+const Thing = require('../models/Sauces');
+const fs = require('fs');
+
+exports.createSauce = (req, res, next) => {
+    const thingObject = JSON.parse(req.body.sauce);
+    delete thingObject._id;
+    const sauce = new Sauce({
+        ...thingObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+    const thingObject = req.file ?
+        {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const fileName = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${fileName}`, () => {
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.getAllSauces = (req, res, next) => {
+    Thing.find()
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(400).json({ error }));
+};
